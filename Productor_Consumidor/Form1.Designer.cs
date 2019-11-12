3f7a5cde5a809779b0b67faf3bd2105ba2302254
Uriel <urielgarciarj@outlook.com>
2019-11-11 22:09:46 -0600

    Terminado1

---
+++ Productor_Consumidor/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Productor_Consumidor
+{
+    partial class Restaurant
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Restaurant));
+            this.pboxTable1 = new System.Windows.Forms.PictureBox();
+            this.pboxTable2 = new System.Windows.Forms.PictureBox();
+            this.pboxTable3 = new System.Windows.Forms.PictureBox();
+            this.pboxTable4 = new System.Windows.Forms.PictureBox();
+            this.pboxTable5 = new System.Windows.Forms.PictureBox();
+            this.pboxTable6 = new System.Windows.Forms.PictureBox();
+            this.pboxTable7 = new System.Windows.Forms.PictureBox();
+            this.pboxTable8 = new System.Windows.Forms.PictureBox();
+            this.pboxChef = new System.Windows.Forms.PictureBox();
+            this.pboxCliente = new System.Windows.Forms.PictureBox();
+            this.button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxChef)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCliente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pboxTable1
+            // 
+            this.pboxTable1.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable1.Image")));
+            this.pboxTable1.Location = new System.Drawing.Point(133, 282);
+            this.pboxTable1.Name = "pboxTable1";
+            this.pboxTable1.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable1.TabIndex = 0;
+            this.pboxTable1.TabStop = false;
+            // 
+            // pboxTable2
+            // 
+            this.pboxTable2.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable2.Image")));
+            this.pboxTable2.Location = new System.Drawing.Point(302, 282);
+            this.pboxTable2.Name = "pboxTable2";
+            this.pboxTable2.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable2.TabIndex = 1;
+            this.pboxTable2.TabStop = false;
+            // 
+            // pboxTable3
+            // 
+            this.pboxTable3.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable3.Image")));
+            this.pboxTable3.Location = new System.Drawing.Point(493, 282);
+            this.pboxTable3.Name = "pboxTable3";
+            this.pboxTable3.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable3.TabIndex = 2;
+            this.pboxTable3.TabStop = false;
+            // 
+            // pboxTable4
+            // 
+            this.pboxTable4.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable4.Image")));
+            this.pboxTable4.Location = new System.Drawing.Point(681, 282);
+            this.pboxTable4.Name = "pboxTable4";
+            this.pboxTable4.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable4.TabIndex = 3;
+            this.pboxTable4.TabStop = false;
+            // 
+            // pboxTable5
+            // 
+            this.pboxTable5.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable5.Image")));
+            this.pboxTable5.Location = new System.Drawing.Point(133, 399);
+            this.pboxTable5.Name = "pboxTable5";
+            this.pboxTable5.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable5.TabIndex = 4;
+            this.pboxTable5.TabStop = false;
+            // 
+            // pboxTable6
+            // 
+            this.pboxTable6.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable6.Image")));
+            this.pboxTable6.Location = new System.Drawing.Point(302, 399);
+            this.pboxTable6.Name = "pboxTable6";
+            this.pboxTable6.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable6.TabIndex = 5;
+            this.pboxTable6.TabStop = false;
+            // 
+            // pboxTable7
+            // 
+            this.pboxTable7.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable7.Image")));
+            this.pboxTable7.Location = new System.Drawing.Point(493, 399);
+            this.pboxTable7.Name = "pboxTable7";
+            this.pboxTable7.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable7.TabIndex = 6;
+            this.pboxTable7.TabStop = false;
+            // 
+            // pboxTable8
+            // 
+            this.pboxTable8.Image = ((System.Drawing.Image)(resources.GetObject("pboxTable8.Image")));
+            this.pboxTable8.Location = new System.Drawing.Point(681, 399);
+            this.pboxTable8.Name = "pboxTable8";
+            this.pboxTable8.Size = new System.Drawing.Size(117, 77);
+            this.pboxTable8.TabIndex = 7;
+            this.pboxTable8.TabStop = false;
+            // 
+            // pboxChef
+            // 
+            this.pboxChef.Image = ((System.Drawing.Image)(resources.GetObject("pboxChef.Image")));
+            this.pboxChef.Location = new System.Drawing.Point(168, 60);
+            this.pboxChef.Name = "pboxChef";
+            this.pboxChef.Size = new System.Drawing.Size(127, 107);
+            this.pboxChef.TabIndex = 8;
+            this.pboxChef.TabStop = false;
+            // 
+            // pboxCliente
+            // 
+            this.pboxCliente.Image = ((System.Drawing.Image)(resources.GetObject("pboxCliente.Image")));
+            this.pboxCliente.Location = new System.Drawing.Point(592, 60);
+            this.pboxCliente.Name = "pboxCliente";
+            this.pboxCliente.Size = new System.Drawing.Size(127, 107);
+            this.pboxCliente.TabIndex = 9;
+            this.pboxCliente.TabStop = false;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(366, 205);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(161, 23);
+            this.button.TabIndex = 10;
+            this.button.Text = "Abrir Restaurante";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Restaurant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 530);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.pboxCliente);
+            this.Controls.Add(this.pboxChef);
+            this.Controls.Add(this.pboxTable8);
+            this.Controls.Add(this.pboxTable7);
+            this.Controls.Add(this.pboxTable6);
+            this.Controls.Add(this.pboxTable5);
+            this.Controls.Add(this.pboxTable4);
+            this.Controls.Add(this.pboxTable3);
+            this.Controls.Add(this.pboxTable2);
+            this.Controls.Add(this.pboxTable1);
+            this.Name = "Restaurant";
+            this.Text = "Restaurante";
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxTable8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxChef)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCliente)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pboxTable2;
+        private System.Windows.Forms.PictureBox pboxTable3;
+        private System.Windows.Forms.PictureBox pboxTable4;
+        private System.Windows.Forms.PictureBox pboxTable5;
+        private System.Windows.Forms.PictureBox pboxTable6;
+        private System.Windows.Forms.PictureBox pboxTable7;
+        private System.Windows.Forms.PictureBox pboxTable8;
+        private System.Windows.Forms.PictureBox pboxChef;
+        private System.Windows.Forms.PictureBox pboxCliente;
+        private System.Windows.Forms.Button button;
+        public System.Windows.Forms.PictureBox pboxTable1;
+    }
+}
+
---
+++ Productor_Consumidor/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Productor_Consumidor
+{
+    public partial class Restaurant : Form
+    {
+        static PictureBox[] characters = new PictureBox[2];
+        static PictureBox[] Tables = new PictureBox[8];
+
+        static Queue<PictureBox> myq = new Queue<PictureBox>();
+
+        static Queue<PictureBox> myqcliente = new Queue<PictureBox>();
+        public Restaurant()
+        {
+
+            InitializeComponent();
+            pboxChef.Hide();
+            pboxCliente.Hide();
+            pboxTable1.Hide();
+            pboxTable2.Hide();
+            pboxTable3.Hide();
+            pboxTable4.Hide();
+            pboxTable5.Hide();
+            pboxTable6.Hide();
+            pboxTable7.Hide();
+            pboxTable8.Hide();
+
+            /*characters[0] = pboxChef;
+            characters[1] = pboxCliente;*/
+
+            /*Tables[0] = pboxTable1;
+            Tables[1] = pboxTable2;
+            Tables[2] = pboxTable3;
+            Tables[3] = pboxTable4;
+            Tables[4] = pboxTable5;
+            Tables[5] = pboxTable6;
+            Tables[6] = pboxTable7;
+            Tables[7] = pboxTable8;*/
+
+            myq.Enqueue(pboxTable1);
+            myq.Enqueue(pboxTable2);
+            myq.Enqueue(pboxTable3);
+            myq.Enqueue(pboxTable4);
+            myq.Enqueue(pboxTable5);
+            myq.Enqueue(pboxTable6);
+            myq.Enqueue(pboxTable7);
+            myq.Enqueue(pboxTable8);
+
+
+            myqcliente.Enqueue(pboxTable1);
+            myqcliente.Enqueue(pboxTable2);
+            myqcliente.Enqueue(pboxTable3);
+            myqcliente.Enqueue(pboxTable4);
+            myqcliente.Enqueue(pboxTable5);
+            myqcliente.Enqueue(pboxTable6);
+            myqcliente.Enqueue(pboxTable7);
+            myqcliente.Enqueue(pboxTable8);
+
+        }
+
+        public static class GlobalData
+        {
+            public static int decision = 0;
+            public static int numberOfActionsChef = 0;
+            public static int numberOfActionsClient = 1;
+            public static int ultimaImagen = 0;
+        }
+
+        public async Task action()
+        {
+            Random rdn = new Random();
+            GlobalData.numberOfActionsChef = rdn.Next(1, 4);
+        }
+
+        public async void productor()
+        {
+            await action();
+            MessageBox.Show(GlobalData.numberOfActionsChef.ToString());
+            pboxCliente.Invoke(new MethodInvoker(delegate
+            {
+                pboxCliente.Hide();
+            }));
+            pboxChef.Invoke(new MethodInvoker(delegate
+            {
+                pboxChef.Show();
+            }));
+
+            for(int i =0; i < GlobalData.numberOfActionsChef; i++)
+            {
+                if(myq.Count == 0)
+                {
+                    llenarCola();
+                }
+                PictureBox ver = myq.Dequeue();
+                ver.Invoke(new MethodInvoker(delegate
+                {
+                    ver.Show();
+                }));
+                await Task.Delay(1000);
+            }
+
+            /*List<PictureBox> ImagenesInvisibles = new List<PictureBox>();
+            for (int i = GlobalData.ultimaImagen; i < Tables.Length - GlobalData.ultimaImagen; i++)
+            {
+                if (Tables[i].Visible == false)
+                {
+                    ImagenesInvisibles.Add(Tables[i]);
+                }
+            }
+            MessageBox.Show(GlobalData.numberOfActionsChef.ToString());
+            for (int i = GlobalData.ultimaImagen; i < GlobalData.numberOfActionsChef; i++)
+            {
+                ImagenesInvisibles[i].Invoke(new MethodInvoker(delegate
+                {
+                    ImagenesInvisibles[i].Show();
+                }));
+                GlobalData.ultimaImagen++; 
+                await Task.Delay(1000);
+            }
+
+            MessageBox.Show(GlobalData.ultimaImagen.ToString());
+            ImagenesInvisibles.Clear();*/
+            cliente();
+    }
+
+        public async void cliente()
+        {
+            pboxChef.Invoke(new MethodInvoker(delegate
+            {
+                pboxChef.Hide();
+            }));
+            pboxCliente.Invoke(new MethodInvoker(delegate
+            {
+                pboxCliente.Show();
+            }));
+            Random rdn = new Random();
+            GlobalData.numberOfActionsClient = rdn.Next(1, GlobalData.numberOfActionsChef);
+            MessageBox.Show(GlobalData.numberOfActionsClient.ToString());
+
+            for (int i = 0; i < GlobalData.numberOfActionsClient; i++)
+            {
+                if (myqcliente.Count == 0)
+                {
+                    llenarCola2();
+                }
+                PictureBox ver = myqcliente.Dequeue();
+                ver.Invoke(new MethodInvoker(delegate
+                {
+                    ver.Hide();
+                }));
+                await Task.Delay(1000);
+            }
+
+            /*
+            List<PictureBox> mesasHechas = new List<PictureBox>();
+            for (int i = 0; i < Tables.Length; i++)
+            {
+                if (Tables[i].Visible == true)
+                {
+                    mesasHechas.Add(Tables[i]);
+                }
+            }
+            for (int i = 0; i < GlobalData.numberOfActionsClient; i++)
+            {
+                mesasHechas[i].Invoke(new MethodInvoker(delegate
+                {
+                    mesasHechas[i].Hide();
+                }));
+                await Task.Delay(1000);
+            }
+            mesasHechas.Clear();*/
+            productor();
+        }
+
+
+        public async void llenarCola()
+        {
+            myq.Enqueue(pboxTable1);
+            myq.Enqueue(pboxTable2);
+            myq.Enqueue(pboxTable3);
+            myq.Enqueue(pboxTable4);
+            myq.Enqueue(pboxTable5);
+            myq.Enqueue(pboxTable6);
+            myq.Enqueue(pboxTable7);
+            myq.Enqueue(pboxTable8);
+        }
+
+        public async void llenarCola2()
+        {
+            myqcliente.Enqueue(pboxTable1);
+            myqcliente.Enqueue(pboxTable2);
+            myqcliente.Enqueue(pboxTable3);
+            myqcliente.Enqueue(pboxTable4);
+            myqcliente.Enqueue(pboxTable5);
+            myqcliente.Enqueue(pboxTable6);
+            myqcliente.Enqueue(pboxTable7);
+            myqcliente.Enqueue(pboxTable8);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Thread productorHilo = new Thread(productor);
+            productorHilo.Start();
+        }
+    }
+}
